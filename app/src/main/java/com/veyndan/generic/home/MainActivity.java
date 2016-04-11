656f3dde909c09cb0c828cbc9594916c803cf0db
veyndan <veyndan89@gmail.com>
2016-04-11 20:47:40 +0100

    Remove view pager

--- app/src/main/java/com/veyndan/generic/home/MainActivity.java
+++ app/src/main/java/com/veyndan/generic/home/MainActivity.java
@@ -1,11 +1,6 @@
 package com.veyndan.generic.home;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,15 +9,11 @@ import com.veyndan.generic.R;
 import com.veyndan.generic.ui.BaseActivity;
 import com.veyndan.generic.util.LogUtils;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
     @SuppressWarnings("unused")
     private static final String TAG = LogUtils.makeLogTag(MainActivity.class);
-
-    @Bind(R.id.viewpager) ViewPager viewPager;
-    @Bind(R.id.sliding_tabs) TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +25,8 @@ public class MainActivity extends BaseActivity {
         ActionBar ab = getSupportActionBar();
         if (ab != null) {
             ab.setTitle(null);
-            ab.hide();
         }
 
-        if (viewPager != null && tabLayout != null) {
-            viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager()));
-            tabLayout.setupWithViewPager(viewPager);
-            // Must do setupWithViewPager before setting icons
-            for (int i = 0; i < tabLayout.getTabCount(); i++) {
-                TabLayout.Tab tab = tabLayout.getTabAt(i);
-                if (tab != null) {
-                    tab.setIcon(R.drawable.ic_home_24dp);
-                }
-            }
-        }
     }
 
     @Override
@@ -65,25 +44,6 @@ public class MainActivity extends BaseActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    static class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
-        final int PAGE_COUNT = 1;
-
-        public SampleFragmentPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public int getCount() {
-            return PAGE_COUNT;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return HomeFragment.newInstance();
-        }
-
     }
 
 }
